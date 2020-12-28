01f902848f71b22e8a2495ccb9afaeb838ea5b53
tresvian <jesustonyruiz@gmail.com>
2020-12-27 20:58:25 -0500

    HTTP handler

--- download_novelfull.py
+++ download_novelfull.py
@@ -11,9 +11,29 @@ import requests
 import argparse
 import logging
 import re
+from html.parser import HTMLParser
 
-def main(first, max, output):
-    pass
+class htmlparser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.tags = []
+        self.next = ''
+    def handle_starttag(self, tag, attrs):
+        #[('class', 'btn btn-success'), ('href', '/beware-mr-officer-tread-carefully/chapter-4.html'), ('title', 'Chapter 4'), ('id', 'next_chap')]
+        if tag == 'span' and attrs != [] and attrs[0] == ('class', 'btn btn-success') and attrs[3][1] == 'next_chap':
+            self.tags.append(0)
+    def handle_endtag(self, tag):
+        pass
+    def handle_data(self, data):
+        if self.tags.count != 0:
+            tag = self.tags.pop()
+            if tag == 0:
+                self.next = data
+            else:
+                logging.error('HTMLParser did not implement correctly on a handle')
+
+def main(first, max, output, novel_name):
+    for x in range(max):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -26,16 +46,18 @@ if __name__ == '__main__':
     if type(args.first_chapter) is list:
         logging.info("Detected multiple requests")
         for x in args.first:
-            name = re.search(r'http[s]?:\/\/novelfull\.com\/(.+)\/',x.lower())
+            name = re.search(r'https?:\/\/novelfull\.com\/(.+)\/',x.lower())
+            novel_name = name[1]
             if name == None:
                 logging.error('Could not parse ' + x + ', skipping')
                 continue
-            logging.info("Pulling " + name[1])
-            main(x, args.max_chapters, args.output_format)
+            logging.info("Pulling " + novel_name)
+            main(x, args.max_chapters, args.output_format, novel_name)
     else:
-        name = re.search(r'http[s]?:\/\/novelfull\.com\/(.+)\/',args.first_chapter.lower())
+        name = re.search(r'https?:\/\/novelfull\.com\/(.+)\/',args.first_chapter.lower())
+        novel_name = name[1]
         if name == None:
             logging.error('Could not parse ' + args.first_chapter + ', quitting')
             exit()
-        logging.info("Pulling " + name[1])
-        main(args.first_chapter, args.max_chapters, args.output_format)
+        logging.info("Pulling " + novel_name)
+        main(args.first_chapter, args.max_chapters, args.output_format, novel_name)
